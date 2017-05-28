df2dee2c17025e171ffc7a9f006fcddd35c7ce89
Benjamin Piatt <benjamin.piatt@outcomehealth.com>
2017-05-27 23:13:18 -0500

    Added launcher activity with custom title bar, added color palette

---
+++ app/src/main/java/com/piatt/udacity/bakeaide/RecipesActivity.java
@@ -0,0 +1,13 @@
+package com.piatt.udacity.bakeaide;
+
+import android.os.Bundle;
+import android.app.Activity;
+
+public class RecipesActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_recipes);
+    }
+}
